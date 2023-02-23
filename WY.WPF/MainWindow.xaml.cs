c00c51457088c2432783ed22eb33d46a739b5128
LuckywyuJK <luckywyu227515@163.com>
2023-02-23 23:24:23 +0800

    测试

--- WY.WPF/MainWindow.xaml.cs
+++ WY.WPF/MainWindow.xaml.cs
@@ -20,9 +20,24 @@ namespace WY.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        //第一步:声明依赖属性
+        //依赖属性必须注册在依赖对象中（继承DependencyObject）
+        public static readonly DependencyProperty VlueProperty;
+        //第二步：注册,通过new PropertyMetadata给依赖属性一个默认值
+        static MainWindow()
+        {
+            VlueProperty = DependencyProperty.Register("VlueProperty", typeof(double),typeof(MainWindow),new PropertyMetadata(1.0,));
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
+        //第三步：属性包装
+        public double Vlue
+        {
+            get { return (double)this.GetValue(VlueProperty); }
+            set { this.SetValue(VlueProperty,1.0); }
+        }
+
     }
 }
